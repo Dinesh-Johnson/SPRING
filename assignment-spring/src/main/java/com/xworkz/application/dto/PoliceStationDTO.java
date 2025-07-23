a8374150bca320e530f8c21048d5f37219bc3b9c
Dinesh-Johnson <dinesh.xworkz@gmail.com>
2025-07-23 14:18:25 +0530

    22-07-2025

---
+++ assignment-spring/src/main/java/com/xworkz/application/dto/PoliceStationDTO.java
@@ -0,0 +1,18 @@
+package com.xworkz.application.dto;
+
+import lombok.Data;
+
+@Data
+public class PoliceStationDTO {
+
+    private String station;
+    private String area;
+    private String city;
+    private String officer;
+    private String contact;
+    private String email;
+    private String staff;
+    private String fir;
+    private String jurisdiction;
+    private String timing;
+}
---
+++ assignment-spring/src/main/java/com/xworkz/application/dto/RTOdto.java
@@ -0,0 +1,14 @@
+package com.xworkz.application.dto;
+
+import lombok.Data;
+
+@Data
+public class RTOdto {
+
+    private String code;
+    private String city;
+    private String officer;
+    private String address;
+    private String staff;
+    private String timings;
+}
